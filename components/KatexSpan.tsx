f7871fee4e644b8344be87099e00dfdb0373ff21
JC <jacob-cooper1@student.yorkcollege.ac.uk>
2026-01-31 18:13:30 +0000

    Fixed Katex Span to actually work in prod

--- components/KatexSpan.tsx
+++ components/KatexSpan.tsx
@@ -5,7 +5,7 @@
 import renderMathInElement from 'katex/dist/contrib/auto-render.js';
 import 'katex/dist/katex.min.css';
 import { useEffect, useRef } from 'react';
-
+//@ts-ignore
 export default function KatexSpan({ text, ...delegated }) {
   const katexTextRef = useRef();
   useEffect(() => {
